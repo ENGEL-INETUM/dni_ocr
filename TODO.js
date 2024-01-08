c789349f71c990fc9a88c03412bc820c7247758b
ENGEL-INETUM <137941335+ENGEL-INETUM@users.noreply.github.com>
2024-01-08 15:48:54 +0100

    prooves

---
+++ TODO.js
@@ -0,0 +1,14 @@
+// TODO:
+
+//  - Investigar otras librerías para que lean perfectamente el texto del DNI Español.
+
+
+
+// ----------------------------------------------------------------------
+// DONE:
+
+//  - Mientras captura ocr mostrar un loading
+//  - Descargar las librerías en local.
+//  - En caso de que todo esté OK saltar un aviso de OK.
+// - En caso de que esté vacío saltar aviso para repetir foto.
+// - En caso de que cada línea no tenga la longitud mínima (30) saltar un aviso de repetir foto.
